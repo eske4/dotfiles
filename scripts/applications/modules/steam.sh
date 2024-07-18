c8a1112ff9c2b807d8ac8b0d1c12382b5a4be6e0
eske4 <eske.larsen@gmail.com>
2024-07-19 00:58:33 +0200

    Add lock screen and auto logout

--- scripts/applications/modules/steam.sh
+++ scripts/applications/modules/steam.sh
@@ -12,22 +12,22 @@ sudo wget -q https://repo.steampowered.com/steam/archive/precise/steam_latest.de
 if [ -f "steam_latest.deb" ]; then
     # Extract the contents of the package
 
-    sudo ar x $SteamFolder/steam_latest.deb
+    sudo ar x "$SteamFolder"/steam_latest.deb
     # Extract control.tar.gz and data.tar.xz
-    sudo tar -xf $SteamFolder/control.tar.gz
-    sudo tar -xf $SteamFolder/data.tar.xz
+    sudo tar -xf "$SteamFolder"/control.tar.gz
+    sudo tar -xf "$SteamFolder"/data.tar.xz
 
-    sudo tar -xvf $SteamFolder/control.tar.gz
-    sudo tar -xvf $SteamFolder/data.tar.xz
+    sudo tar -xvf "$SteamFolder"/control.tar.gz
+    sudo tar -xvf "$SteamFolder"/data.tar.xz
 
     # Copy files to relevant system directories
-    sudo cp -r $SteamFolder/etc/* /etc
-    sudo cp -r $SteamFolder/lib/* /lib
-    sudo cp -r $SteamFolder/usr/* /usr
+    sudo cp -r "$SteamFolder"/etc/* /etc
+    sudo cp -r "$SteamFolder"/lib/* /lib
+    sudo cp -r "$SteamFolder"/usr/* /usr
 
     # Clean up temporary files
-    sudo rm $SteamFolder/steam_latest.deb
-    sudo rm -rf $SteamFolder
+    sudo rm "$SteamFolder"/steam_latest.deb
+    sudo rm -rf "$SteamFolder"
 
     echo "Steam has been installed successfully."
 else
