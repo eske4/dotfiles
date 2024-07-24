8e25203a694561f73172b272cd41da912bd52e28
eske4 <eske.larsen@gmail.com>
2024-07-24 12:13:41 +0200

    Added task manager and scripts for hyprland plugins to be installed(needs work)

---
+++ scripts/applications/modules/better-discord.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 1 | yay -S --noconfirm betterdiscord
+discord
+betterdiscordctl -D /opt/BetterDiscord install
---
+++ scripts/hyprland/plugins.sh
@@ -0,0 +1,4 @@
+
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+hyprpm enable hyprexpo
+hyprpm enable hyprbars
