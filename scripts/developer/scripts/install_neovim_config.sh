d8b635748c4ac0d3f64ca75586595987981d2e3a
eske4 <eske.larsen@gmail.com>
2024-07-28 23:18:42 +0200

    Auto install script for neovim config

---
+++ scripts/developer/scripts/install_neovim_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TARGET_DIR="$HOME/.config/nvim"
+git clone https://github.com/eske4/klintoe-nvim-setup.git "$TARGET_DIR"
+"$TARGET_DIR/install.sh"
+
--- scripts/script_collector.sh
+++ scripts/script_collector.sh
@@ -8,4 +8,5 @@ bash "${SCRIPT_DIR}/applications/install_applications.sh"
 bash "${SCRIPT_DIR}/service_manager/enable_services.sh"
 bash "${SCRIPT_DIR}/linking/link_config.sh"
 bash "${SCRIPT_DIR}/misc/switch_shell.sh"
+bash "${SCRIPT_DIR}/developer/install.sh"
 
