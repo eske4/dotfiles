c37237041bd2537ccd9b0931507a119ae09e8273
eske4 <eske.larsen@gmail.com>
2024-07-30 23:14:03 +0200

    Changed the order of install packages

--- scripts/packages/install_packages.sh
+++ scripts/packages/install_packages.sh
@@ -6,11 +6,11 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 # Install yay
 bash "${SCRIPT_DIR}/core/install_yay.sh"
 
-# Install development packages
-bash "${SCRIPT_DIR}/utility/install_packages.sh" "${SCRIPT_DIR}/packages/development_packages.txt"
-
 # Install rustup toolchains
 bash "${SCRIPT_DIR}/utility/install_rustup_tools.sh" "${SCRIPT_DIR}/packages/development_packages.txt"
+
+# Install development packages
+bash "${SCRIPT_DIR}/utility/install_packages.sh" "${SCRIPT_DIR}/packages/development_packages.txt"
 
 # Install system packages
 bash "${SCRIPT_DIR}/utility/install_packages.sh" "${SCRIPT_DIR}/packages/system_packages.txt"
