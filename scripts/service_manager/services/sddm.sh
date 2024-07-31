379fca4dcae12666e5f8e26bec51ef2ff3f65b04
eske4 <eske.larsen@gmail.com>
2024-07-31 15:47:21 +0200

    Fixed custom sddm setup

--- scripts/service_manager/services/sddm.sh
+++ scripts/service_manager/services/sddm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sudo systemctl enable sddm
 
-sudo cp -r "$HOME/dotfiles/home/.config/sddm/sugar-dark/" "/usr/share/sddm/themes/sugar-dark/"
+sudo rm -rf "/usr/share/sddm/themes/sugar-dark"
+yay -S sddm-sugar-dark
+sudo rm -rf "/usr/share/sddm/themes/sugar-dark"
+
+sudo mkdir -p /etc/sddm.conf.d
+
+sudo mkdir -p /usr/share/sddm/themes/sugar-dark
+sudo cp -rT "$HOME/dotfiles/home/.config/sddm/sugar-dark" "/usr/share/sddm/themes/sugar-dark"
+
 sudo cp -r "$HOME/dotfiles/home/.config/sddm/sddm.conf" "/etc/sddm.conf.d/sddm.conf"
