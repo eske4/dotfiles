8441c556ee91b304261446bdc5c3d5a69341ad6b
eske4 <eske.larsen@gmail.com>
2024-07-31 17:31:18 +0200

    Bugfix folder structure script typo

--- scripts/misc/setup_misc.sh
+++ scripts/misc/setup_misc.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
 bash "${SCRIPT_DIR}/scripts/switch_shell.sh"
-bash "${SCRIPT_DIR}/scripts/setup_folder_structures"
+bash "${SCRIPT_DIR}/scripts/setup_folder_structures.sh"
 
