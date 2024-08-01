de21aad15c171b3910d7e39612cefbd92dabf689
eske4 <eske.larsen@gmail.com>
2024-08-01 22:55:11 +0200

    Fixed sddm script bug

--- scripts/service_manager/services/sddm.sh
+++ scripts/service_manager/services/sddm.sh
@@ -4,8 +4,7 @@ sudo systemctl enable sddm
 sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-svg
 
 sudo mkdir -p /etc/sddm.conf.d
-sudo mkdir -p /usr/share/sddm/themes/sugar-dark
 
-sudo ln -sfn "$HOME/dotfiles/home/.config/sddm/sugar-dark" "/usr/share/sddm/themes/sugar-dark"
+sudo ln -sfn "$HOME/dotfiles/home/.config/sddm/sugar-dark" "/usr/share/sddm/themes"
 sudo ln -sfn "$HOME/dotfiles/home/.config/sddm/sddm.conf" "/etc/sddm.conf.d/sddm.conf"
 
