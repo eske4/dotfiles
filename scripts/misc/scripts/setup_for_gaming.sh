122435fb23223f27dc4a4c0630033a1c32f6ff20
eske4 <eske.larsen@gmail.com>
2024-08-04 00:18:39 +0200

    Added a script to make arch linux easier to setup for gaming

---
+++ scripts/misc/scripts/setup_for_gaming.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm timeshift lib32-vkd3d vkd3d gamemode lib32-gamemode wine wine-gecko wine-mono freetype2 flatpak v4l2loopback-dkms v4l2loopback-utils v4l-utils pipewire-v4l2 lib32-pipewire-v4l2 mangohud gamescope goverlay
+yay -S protonup-qt --noconfirm
+
